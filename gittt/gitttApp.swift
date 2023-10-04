9e7b47e1bbd1907e8b6ce2736544ee0dab287671
daolkong <daolkong06@gmail.com>
2023-10-04 20:28:21 +0900

    Initial Commit

---
+++ gittt/gitttApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitttApp.swift
+//  gittt
+//
+//  Created by Daol on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct gitttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
